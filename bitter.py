6f5be2d33272d2dfe6edc333f0aeeedc5d760416
srathbun <spencerrathbun@gmail.com>
2012-05-01 08:44:30 -0400

    now working, just need to adjust the input numbers and check that delete functions

--- bitter.py
+++ bitter.py
@@ -17,15 +17,15 @@ def printVals(stdscr):
 	global currField, top, bottom, total
 	total["value"] = eval("{:} {:} {:}".format(top["value"], bitop, bottom["value"]))
 	stdscr.addstr("------------------\n")
-	stdscr.addstr("|{:^16}| {:}\n".format(top["value"], top["value"]))
+	stdscr.addstr("|{:^16}| {:}\n".format(bin(top["value"])[2:], top["value"]))
 	stdscr.addstr("------------------\n")
 	stdscr.addstr("         {0}        \n".format(bitop))
 	stdscr.addstr("------------------\n")
-	stdscr.addstr("|{:^16}| {:}\n".format(bottom["value"], bottom["value"]))
+	stdscr.addstr("|{:^16}| {:}\n".format(bin(bottom["value"])[2:], bottom["value"]))
 	stdscr.addstr("------------------\n")
-	stdscr.addstr("\n")
+	stdscr.addstr("          =       \n")
 	stdscr.addstr("------------------\n")
-	stdscr.addstr("|{:^16}| {:}\n".format(total["value"], total["value"]))
+	stdscr.addstr("|{:^16}| {:}\n".format(bin(total["value"])[2:], total["value"]))
 	stdscr.addstr("------------------\n")
 	stdscr.move(currField["position"][0], currField["position"][1])
 	stdscr.cursyncup()
@@ -37,9 +37,19 @@ def switchField():
 	else:
 		currField = top
 
+def switchBitOp():
+	global bitop
+	ops = [ "&", "^", "|", "<<", ">>" ]
+	current = ops.index(bitop)
+	if current == len(ops)-1:
+		bitop = ops[0]
+	else:
+		bitop = ops[current+1]
+
 def doAction(key):
 	if key == curses.ascii.TAB: switchField()
 	elif curses.ascii.isdigit(key): currField["value"] += int(curses.ascii.unctrl(key))
+	elif key == curses.ascii.SP: switchBitOp()
 	elif key == ord('q'): return True # Exit the while()
 	elif key == ord('Q'): return True # Exit the while()
 	return False
